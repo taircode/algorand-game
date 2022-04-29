1a8882b27cb21d190b0f10c0224aa97167a6e341
taircode <t.akhmejanov@gmail.com>
2022-04-29 15:04:00 -0500

    adjusted global_bytes and get_approvale_program

---
+++ deploy_app.py
@@ -0,0 +1,140 @@
+from pyteal import *
+from algosdk import account, algod, transaction, mnemonic
+import tic_tac_toe
+import base64
+
+"""
+Following the example here https://developer.algorand.org/docs/get-details/dapps/pyteal/
+to deploy and call contract
+"""
+
+# helper function to compile program source
+def compile_program(client, source_code):
+    compile_response = client.compile(source_code)
+    return base64.b64decode(compile_response['result'])
+
+# helper function that converts a mnemonic passphrase into a private signing key
+def get_private_key_from_mnemonic(mn) :
+    private_key = mnemonic.to_private_key(mn)
+    return private_key
+
+# helper function that formats global state for printing
+def format_state(state):
+    formatted = {}
+    for item in state:
+        key = item['key']
+        value = item['value']
+        formatted_key = base64.b64decode(key).decode('utf-8')
+        if value['type'] == 1:
+            # byte string
+            if formatted_key == 'voted':
+                formatted_value = base64.b64decode(value['bytes']).decode('utf-8')
+            else:
+                formatted_value = value['bytes']
+            formatted[formatted_key] = formatted_value
+        else:
+            # integer
+            formatted[formatted_key] = value['uint']
+    return formatted
+
+# helper function to read app global state
+def read_global_state(client, app_id):
+    app = client.application_info(app_id)
+    app_params = app['params'] #what are all of the params?
+    global_state = app['params']['global-state'] if "global-state" in app['params'] else []
+    return format_state(global_state)
+
+"""
+This is the main function to call in order to deploy the smart contract
+"""
+# create new application. This function creates the application creation transaction, signs, and sends it.
+def create_app(client, private_key, approval_program, clear_program, global_schema, local_schema):
+    # define sender as creator
+    sender = account.address_from_private_key(private_key)
+
+    # declare on_complete as NoOp
+    on_complete = transaction.OnComplete.NoOpOC.real
+
+    # get node suggested parameters
+    params = client.suggested_params()
+
+    # create unsigned transaction
+    txn = transaction.ApplicationCreateTxn(sender, params, on_complete, approval_program, clear_program, global_schema, local_schema)
+
+    # sign transaction
+    signed_txn = txn.sign(private_key)
+    tx_id = signed_txn.transaction.get_txid()
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    # wait for confirmation
+    try:
+        transaction_response = transaction.wait_for_confirmation(client, tx_id, 4)
+        print("TXID: ", tx_id)
+        print("Result confirmed in round: {}".format(transaction_response['confirmed-round']))
+
+    except Exception as err:
+        print(err)
+        return
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    app_id = transaction_response['application-index']
+    print("Created new app-id:", app_id)
+
+    return app_id
+
+
+# user declared account mnemonics
+#you should never save this like this in production
+#figure out a safer way to do this
+creator_mnemonic = "REPLACE WITH YOUR OWN MNEMONIC" 
+# user declared algod connection parameters.
+# Node must have EnableDeveloperAPI set to true in its config
+algod_address = "http://localhost:4001"
+algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+"""
+Main script to create the app on the blockchain and then view its global state
+"""
+
+if __name__=='__main__':
+    # initialize an algodClient
+    algod_client = algod.AlgodClient(algod_token, algod_address)
+
+    # define private keys
+    creator_private_key = get_private_key_from_mnemonic(creator_mnemonic)
+
+    # declare application state storage (immutable)
+    local_ints = 0
+    local_bytes = 0
+    global_ints = 0 #had to change these to work for our tic-tac-toe example
+    global_bytes = 12 #had to change these to work for our tic-tac-toe example
+    global_schema = transaction.StateSchema(global_ints, global_bytes)
+    local_schema = transaction.StateSchema(local_ints, local_bytes)
+
+    # compile program to TEAL assembly
+    with open("./approval.teal", "w") as f:
+        approval_program_teal = tic_tac_toe.get_approval_program()
+        f.write(approval_program_teal) #we don't techinically need to write it right? this is just to have a record?
+
+    # compile program to TEAL assembly
+    with open("./clear.teal", "w") as f:
+        clear_state_program_teal = tic_tac_toe.get_clear_state_program()
+        f.write(clear_state_program_teal) #we don't techinically need to write it right? this is just to have a record?
+
+    # compile program to binary from teal source code
+    approval_program_compiled = compile_program(algod_client, approval_program_teal)
+
+    # compile program to binary from teal source code
+    clear_state_program_compiled = compile_program(algod_client, clear_state_program_teal)
+
+    print("--------------------------------------------")
+    print("Deploying Tic-Tac-Toe application......")
+
+    # create new application
+    app_id = create_app(algod_client, creator_private_key, approval_program_compiled, clear_state_program_compiled, global_schema, local_schema)
+
+    # read global state of application
+    print("Global state:", read_global_state(algod_client, app_id))
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,20 +1,24 @@
 from pyteal import *
 
+"""
+A simple game of tic-tac-toe
+Later add functionality for a wager -or- receiving an NFT stating that winner won the game
+"""
 def play_tic_tac_toe():
 
     handle_creation = Seq(
         App.globalPut(Bytes("creator"), Txn.sender()), #creator will be O's
         App.globalPut(Bytes("guest"), Txn.application_args[0]), #invited_guest will be X's
         App.globalPut(Bytes("whose_turn"), Txn.application_args[0]), #invited_guest will go first
-        App.globalPut(Bytes("N"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("E"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("S"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("W"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("NE"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("SE"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("SW"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("NW"), Int(0)), # write a byte slice
-        App.globalPut(Bytes("C"), Int(0)), # write a byte slice
+        App.globalPut(Bytes("N"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("E"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("S"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("W"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("NE"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("SE"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("SW"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("NW"), Bytes("empty")), # write a byte slice
+        App.globalPut(Bytes("C"), Bytes("empty")), # write a byte slice
         Return(Int(1)) # Could also be Approve()
     )
 
@@ -24,15 +28,17 @@ def play_tic_tac_toe():
 
     handle_updateapp = Return(Int(0)) # Could also be Reject()
 
-    handle_deleteapp = Return(Int(0)) # Could also be Reject()
+    handle_deleteapp = Return(Int(1)) #what if this is 0? can you just never delete the app then?
 
     flip_whose_turn = If(App.globalGet(Bytes("whose_turn"))==App.globalGet(Bytes("creator")),App.globalPut(Bytes("whose_turn"), App.globalGet(Bytes("guest"))),App.globalPut(Bytes("whose_turn"), App.globalGet(Bytes("creator"))))
 
     #payout some money or something.
     declare_winner = Return(Int(1))
+    #clear board to play again
 
     #return an even split of the money
     no_winner = Return(Int(1))
+    #clear board to play again
 
     #need to implement a check to see if someone won
     #check_if_winner = 
@@ -53,21 +59,21 @@ def play_tic_tac_toe():
             Txn.application_args[0]==Bytes("C"),
             )
         ),
-        Assert(App.globalGet(Txn.application_args[0])==Int(0)), #fail if the location you are trying to mark has already been marked
+        Assert(App.globalGet(Txn.application_args[0])==Bytes("empty")), #fail if the location you are trying to mark has already been marked
         #mark the squre X if guest, O if creator
         If(App.globalGet(Bytes("whose_turn"))==App.globalGet(Bytes("guest")),App.globalPut(Txn.application_args[0],Bytes("X")),App.globalPut(Txn.application_args[0],Bytes("O"))),
         #you should check if someone got three in a row
         #check_if_winner, if so, declare_winner
         If(Or(#check to see if board is full
-            App.globalGet(Bytes("N"))==Int(0),
-            App.globalGet(Bytes("E"))==Int(0),
-            App.globalGet(Bytes("S"))==Int(0),
-            App.globalGet(Bytes("W"))==Int(0),
-            App.globalGet(Bytes("NE"))==Int(0),
-            App.globalGet(Bytes("SE"))==Int(0),
-            App.globalGet(Bytes("SW"))==Int(0),
-            App.globalGet(Bytes("NW"))==Int(0),
-            App.globalGet(Bytes("C"))==Int(0)
+            App.globalGet(Bytes("N"))==Bytes("empty"),
+            App.globalGet(Bytes("E"))==Bytes("empty"),
+            App.globalGet(Bytes("S"))==Bytes("empty"),
+            App.globalGet(Bytes("W"))==Bytes("empty"),
+            App.globalGet(Bytes("NE"))==Bytes("empty"),
+            App.globalGet(Bytes("SE"))==Bytes("empty"),
+            App.globalGet(Bytes("SW"))==Bytes("empty"),
+            App.globalGet(Bytes("NW"))==Bytes("empty"),
+            App.globalGet(Bytes("C"))==Bytes("empty")
         ),flip_whose_turn,no_winner),
         Return(Int(1))
     )
@@ -80,6 +86,14 @@ def play_tic_tac_toe():
         [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
         [Txn.on_completion() == OnComplete.NoOp, handle_noop]
     )
+
+def get_approval_program():
+    program = play_tic_tac_toe()
+    return compileTeal(program, mode=Mode.Application, version=3)
+
+def get_clear_state_program():
+    program = Return(Int(1))
+    return compileTeal(program, Mode.Application, version=5)
     
 if __name__ == "__main__":
     program = play_tic_tac_toe()
