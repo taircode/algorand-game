18da0af0eba609ed6b9a6e6e83342ad6d7206076
taircode <t.akhmejanov@gmail.com>
2022-05-01 16:14:21 -0500

    added graphics to view_state

--- view_state.py
+++ view_state.py
@@ -32,3 +32,72 @@ if __name__=="__main__":
     # read global state of application
     global_state=read_global_state(algod_client, app_id)
     print("Global state:", global_state)
+
+    win = GraphWin("Current Board",600,600) 
+    message = Text(Point(win.getWidth()/2, 580), 'Click anywhere to quit.')
+    message.draw(win)
+
+    creator_address=global_state['creator']
+    creator = Text(Point(win.getWidth()/2, 500), 'O: Creator is '+creator_address)
+    creator.draw(win)
+
+    guest_address=global_state['guest']
+    guest = Text(Point(win.getWidth()/2, 515), 'X: Guest is '+guest_address)
+    guest.draw(win)
+
+    winner=global_state['winner']
+    winner_text=Text(Point(win.getWidth()/2, 410), 'The winner is '+winner)
+    winner_text.draw(win)
+
+    if winner=='pending':
+        turn_address=global_state['whose_turn']
+        if turn_address==guest_address:
+            whose_turn= Text(Point(win.getWidth()/2, 390), 'It is X\'s turn.')
+        else:
+            whose_turn= Text(Point(win.getWidth()/2, 390), 'It is O\'s turn.')
+        whose_turn.draw(win)
+    
+    #draw the grid
+    x_offset=10
+    y_offset=10
+    rect = Rectangle(Point(x_offset, y_offset), Point(360+x_offset, 360+y_offset))
+    vert1=Line(Point(x_offset+120,y_offset),Point(x_offset+120,360+y_offset))
+    vert2=Line(Point(x_offset+240,y_offset),Point(x_offset+240,360+y_offset))
+    hor1=Line(Point(x_offset,y_offset+120),Point(x_offset+360,y_offset+120))
+    hor2=Line(Point(x_offset,y_offset+240),Point(x_offset+360,y_offset+240))
+    rect.draw(win)
+    vert1.draw(win)
+    vert2.draw(win)
+    hor1.draw(win)
+    hor2.draw(win)
+
+    grid_points=dict(NW=Point(x_offset+60,y_offset+60),
+    N=Point(x_offset+180,y_offset+60),
+    NE=Point(x_offset+300,y_offset+60),
+
+    W=Point(x_offset+60,y_offset+180),
+    C=Point(x_offset+180,y_offset+180),
+    E=Point(x_offset+300,y_offset+180),
+
+    SW=Point(x_offset+60,y_offset+300),
+    S=Point(x_offset+180,y_offset+300),
+    SE=Point(x_offset+300,y_offset+300)
+    )
+
+    for key, val in global_state.items():
+        if key not in ['whose_turn','creator','guest','winner']:
+            if val!='empty':
+                if val=='X':
+                    #draw an X at location key
+                    x_center=grid_points[key].getX()
+                    y_center=grid_points[key].getY()
+                    line1=Line(Point(x_center-12.5,y_center-12.5), Point(x_center+12.5,y_center+12.5))
+                    line2=Line(Point(x_center+12.5,y_center-12.5), Point(x_center-12.5,y_center+12.5))
+                    line1.draw(win)
+                    line2.draw(win)
+                else:
+                    #draw an O at location key
+                    cir = Circle(grid_points[key], 25)
+                    cir.draw(win)
+    win.getMouse() # Pause to view result
+    win.close()
